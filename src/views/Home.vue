2813674ab1c1bc7b39309c1a103640249fd52ee9
dzivenu <makafuigdzivenu@gmail.com>
2019-11-24 15:27:44 +0000

    -- i

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/steem mobile wordmark.png" class="logo">
+    <div class="message">Steem Mobile is a <a href="https://steemstudios.com" target="_blank">Steem Studios</a> production</div>
+    <div class="small">Site is under construction, please check back later.</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+.logo {
+  width: 30rem;
+}
+.small {
+  margin-top: 1rem;
+  font-size: 80%;
+}
+</style>
